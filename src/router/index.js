cb00c2860c58e4f39a75d5c32e223d9a161ecc19
eeeunjin <eeeunjin1007@gmail.com>
2025-07-22 17:15:54 +0900

    Feat : 로그인, 회원가입 페이지 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,54 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-// 라우터 모듈 import
-// import authRoutes from './auth';
-// import assetRoutes from './asset';
-// import homeRoutes from './home';
-// import mypageRoutes from './mypage';
-// import notificationRoutes from './notification';
-// import policyRoutes from './policy';
-
-// import DefaultLayout from '@/components/layouts/DefaultLayout.vue';
-
 import LoginPage from '@/pages/auth/LoginPage.vue';
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: [
-//     { path: '/', name: 'login', component: LoginPage }, // 첫 화면을 로그인 페이지로
-//     ...authRoutes,
-//     ...assetRoutes,
-//     ...homeRoutes,
-//     ...mypageRoutes,
-//     ...notificationRoutes,
-//     ...policyRoutes,
-//   ],
-// });
+import FindIdPage from '@/pages/auth/FindIdPage.vue';
+import FindPasswordPage from '@/pages/auth/FindPasswordPage.vue';
+import SignUpEmailVerifyPage from '@/pages/auth/SignUpEmailVerifyPage.vue';
+import SignUpProfilePage from '@/pages/auth/SignUpProfilePage.vue';
+import FindIdResultPage from '@/pages/auth/FindIdResultPage.vue';
+import ResetPasswordPage from '@/pages/auth/ResetPasswordPage.vue';
+import AttendanceCheckModal from '@/pages/auth/AttendanceCheckModal.vue';
 
 const routes = [
   {
     path: '/',
     name: 'login',
     component: LoginPage,
+  },
+  {
+    path: '/findId',
+    name: 'findId',
+    component: FindIdPage,
+  },
+  {
+    path: '/findPassword',
+    name: 'findPassword',
+    component: FindPasswordPage,
+  },
+  {
+    path: '/signUpEmailVerify',
+    name: 'signUpEmailVerify',
+    component: SignUpEmailVerifyPage,
+  },
+  {
+    path: '/signUpProfile',
+    name: 'signUpProfile',
+    component: SignUpProfilePage,
+  },
+  {
+    path: '/resetPassword',
+    name: 'resetPassword',
+    component: ResetPasswordPage,
+  },
+  {
+    path: '/findIdResult',
+    name: 'findIdResult',
+    component: FindIdResultPage,
+  },
+  {
+    path: '/attendanceCheck',
+    name: 'attendanceCheck',
+    component: AttendanceCheckModal,
   },
 ];
 
