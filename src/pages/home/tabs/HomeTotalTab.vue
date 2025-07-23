4a6b1e7abbf6fda39c6ce5179719c019805770a9
parksungbin <sungbin931@gmail.com>
2025-07-23 10:09:27 +0900

    feat: 홈 탭별 화면 연결 (전체, 계좌, 적금, 지출)

--- src/pages/home/tabs/HomeTotalTab.vue
+++ src/pages/home/tabs/HomeTotalTab.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="home-wrapper">
-    <h1>홈 메인 탭</h1>
-    <!-- 여기에 AssetSummaryCard 등 구성 요소 넣기 -->
+  <div>
+    <h2>전체 탭입니다.</h2>
   </div>
 </template>
-
-<script setup>
-// 필요 시 컴포넌트 import
-</script>
-
-<style scoped>
-.home-wrapper {
-  padding: 20px;
-}
-</style>
